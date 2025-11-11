eb64d78a8d5a09768abedf30ca181bb816494ca9
moundher122 <m.bouroumana@esi-sba.dz>
2025-11-11 15:48:26 +0100

    add something i will figure out how to inject db after this comiit

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -1,1 +1,26 @@
 package api
+import (
+	"gorm.io/gorm"
+	"metabeat/internal/models"
+	"net/http"
+	"encoding/json"
+)
+
+func RegisterHandler(DB *gorm.DB, w http.ResponseWriter, r *http.Request) (models.User, error) {
+	var user models.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return models.User{}, err
+	}
+	if err := DB.Create(&user).Error; err != nil {
+	if err != nil {
+		return models.User{}, err
+	}
+	return user, nil
+}
+	result := DB.Create(&user)
+	if result.Error != nil {
+		return models.User{}, result.Error
+	}
+	return user, nil
+}
