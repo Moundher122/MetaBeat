727d2bb944307f5b54668e232323b7842a4cf02b
moundher122 <m.bouroumana@esi-sba.dz>
2025-11-11 15:47:17 +0100

    add something i will figure out how to inject db after this comiit

--- cmd/main.go
+++ cmd/main.go
@@ -10,5 +10,8 @@ func main() {
 	DB := db.InitDB()
 	DB.Migrate()
 	http.HandleFunc("/health", api.HealthCheckHandler)
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		api.RegisterHandler(DB.Db, w, r)
+	})
 	http.ListenAndServe(":8080", nil)
 }
